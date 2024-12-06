32f027b0431c46140c056afc407fe4ed0cdfa3c3
hdensity <sam@schm.it>
2024-12-06 20:17:03 +0100

    Added GetOsInfo

---
+++ src/Microsoft.Dism.Tests/GetOsInfoTest.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class GetOsInfoTest : DismTestBase
+    {
+        public GetOsInfoTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Fact]
+        public void GetOnlineOsInfo()
+        {
+            using (DismSession session = DismApi.OpenOnlineSession())
+            {
+                DismOsInfo osInfo = DismApi.GetOsInfo(session);
+
+                osInfo.ShouldNotBeNull();
+                osInfo.OsState.ShouldBe(DismOsState.Online);
+                osInfo.WindowsDirectory.ShouldNotBeNullOrWhiteSpace();
+                osInfo.BootDrive.ShouldNotBeNullOrWhiteSpace();
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Dism/DismApi.GetOsInfo.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Dism
+{
+    public static partial class DismApi
+    {
+        /// <summary>
+        /// Gets detailed OS information for the image.
+        /// </summary>
+        /// <param name="session">A valid DISM Session. The DISM Session must be associated with an image. You can associate a session with an image by using the DismOpenSession Function.</param>
+        /// <returns>A <see cref="DismOsInfo" /> object.</returns>
+        public static DismOsInfo GetOsInfo(DismSession session)
+        {
+            int hresult = NativeMethods.DismGetOsInfo(session, out IntPtr osInfoPtr);
+
+            try
+            {
+                DismUtilities.ThrowIfFail(hresult, session);
+
+                return new DismOsInfo(osInfoPtr.ToStructure<DismApi.DismOsInfo_>());
+            }
+            finally
+            {
+                Delete(osInfoPtr);
+            }
+        }
+
+        internal static partial class NativeMethods
+        {
+            /// <summary>
+            /// Gets detailed OS information for the iamge.
+            /// </summary>
+            /// <param name="session">A valid DISM Session. The DISM Session must be associated with an image. You can associate a session with an image by using the DismOpenSession Function.</param>
+            /// <param name="osInfoPtr">A pointer that will receive the <see cref="DismOsInfo"/> struct.</param>
+            /// <returns>Returns <c>S_OK</c> on success.</returns>
+            [DllImport(DismDllName, EntryPoint = "_DismGetOsInfo")]
+            [return: MarshalAs(UnmanagedType.Error)]
+            public static extern int DismGetOsInfo(DismSession session, out IntPtr osInfoPtr);
+        }
+    }
+}
